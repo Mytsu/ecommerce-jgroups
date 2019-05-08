541bdca67cbde2186e3fbdd90ea2d0d708c430ed
Jonathan Arantes <tlc.jooker@gmail.com>
2019-05-08 14:53:03 -0300

    first commit

---
+++ src/main/java/core/App.java
@@ -0,0 +1,63 @@
+package core;
+
+import java.util.Scanner;
+
+import org.jgroups.JChannel;
+import org.jgroups.Message;
+import org.jgroups.ReceiverAdapter;
+
+/**
+ * Hello world!
+ *
+ */
+public class App extends ReceiverAdapter {
+
+    JChannel channel;
+    String username = "Jonathan";
+
+    private void start() throws Exception {
+        channel = new JChannel();
+        channel.setReceiver(this);
+        channel.connect("ChatGroup");
+            eventLoop();
+        channel.close();
+    }
+
+    private void eventLoop() {
+        Scanner keyboard = new Scanner(System.in);
+        String line = "";
+
+        boolean loop = true;
+        while( loop ) {
+            System.out.print("> ");
+            System.out.flush();
+
+            try {
+                line = keyboard.nextLine().toLowerCase();
+
+                if(line.startsWith("quit") || line.startsWith("exit")) {
+                    loop = false;
+                }
+                else {
+                    line = "[" + username + "]" + line;
+                    Message msg = new Message(null, line);
+                    channel.send(msg);
+                }
+            }
+            catch(Exception e) {
+                // TODO tratar erros na mensagem
+            }
+        }
+
+        keyboard.close();
+    }
+
+    public static void main(String[] args) {
+        try {
+            new App().start();
+        }
+        catch (Exception e) {
+            // do nothing
+        }
+    }
+}
