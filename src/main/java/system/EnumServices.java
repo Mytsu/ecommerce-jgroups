9d8a209c8e84c17f1015ccdee45d212a71628261
dudu-miranda <eduardokira08@gmail.com>
2019-06-13 18:12:16 -0300

    Correcao do enum para facilidade de adicionamento.

--- src/main/java/system/EnumServices.java
+++ src/main/java/system/EnumServices.java
@@ -4,6 +4,62 @@ package system;
 public enum EnumServices {
 	
 	//Servicos da visao pro controle
+	CREATE_CUSTOMER,
+	CREATE_SELLER,
+	LOGIN_CUSTOMER,
+	LOGIN_SELLER,
+	LIST_ITENS,
+	MAKE_QUESTION,
+	BUY_ITEM,
+	SEND_ANSWER,
+	SEARCH_ITEM,
+	ADD_ITEM,
+	SOLD_ITENS,
+	BOUGHT_ITENS,
+	
+	//Servicos do controle pro modelo
+	GET_ITENS,
+	GET_CUSTOMERS,
+	GET_SELLERS,
+	GET_ITEM,
+	GET_CUSTOMER,
+	GET_SELLER,
+	SAVE_ITEM,
+	SAVE_CUSTOMER,
+	SAVE_SELLER,
+	
+	// Servicos da compra de um produto
+	ITEM_EXIST,
+	CUSTOMER_EXIST,
+	SELLER_EXIST,
+	HAS_ENOUGHT_ITEM,
+	HAS_ENOUGHT_FUNDS,
+	
+	ADD_FUNDS_SELLER,
+	ADD_FUNDS_CUSTOMER,
+	DEDUCE_AMOUNT_PRODUCT,
+	ADD_SELL_CUSTOMER,
+	ADD_SELL_SELLER,
+	
+	// Servicos de login
+	CONFIRM_LOGIN_CUSTOMER,
+	CONFIRM_LOGIN_SELLER,
+	
+	// Servicos de pergunta
+	SAVE_QUESTION,
+	SAVE_ANSWER,	
+	
+	//Servicos para montagem dos subgrupos
+	NEW_VIEW_MEMBER,
+	NEW_CONTROL_MEMBER,
+	NEW_MODEL_MEMBER,
+	
+	//Servicos do sistema
+	TOTAL_FUNDS_BOOL,
+	TOTAL_FUNDS_INT;
+
+	/*
+	//Servicos da visao pro controle
 	CREATE_CUSTOMER(1),
 	CREATE_SELLER(2),
 	LOGIN_CUSTOMER(3),
@@ -47,9 +103,7 @@ public enum EnumServices {
 	
 	// Servicos de pergunta
 	SAVE_QUESTION(52),
-	SAVE_ANSWER(53),
-	
-	
+	SAVE_ANSWER(53),	
 	
 	//Servicos para montagem dos subgrupos
 	NEW_VIEW_MEMBER(61),
@@ -58,9 +112,7 @@ public enum EnumServices {
 	
 	//Servicos do sistema
 	TOTAL_FUNDS_BOOL(91),
-	TOTAL_FUNDS_INT(92)
-	;
-	
+	TOTAL_FUNDS_INT(92);
 	
 	private final int value;
 	EnumServices(int requestValue) {
@@ -70,6 +122,7 @@ public enum EnumServices {
 	public int getValue() {
 		return value;
 	}
+	*/
 			
 }
 
