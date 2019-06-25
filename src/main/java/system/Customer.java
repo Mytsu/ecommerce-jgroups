f8d3361f89b9f8b3bfb741e8e5bb276fbd520ffb
dudu-miranda <eduardokira08@gmail.com>
2019-06-25 11:07:08 -0300

    Feita a mudança da classe abstrata.

--- src/main/java/system/Customer.java
+++ src/main/java/system/Customer.java
@@ -1,12 +1,47 @@
 package system;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 
-public class Customer extends User implements Serializable {
+public class Customer implements Serializable {
 
     private static final long serialVersionUID = 4506509784967298618L;
 
+    // Atributos comuns de cliente e vendedor
+    public final String id;
+    public String username, fullname, password;
+    public ArrayList<Sell> sell;
+    public double funds;
+    
     public Customer(String id, String fullname, String password) {
-        super(id, fullname, password);
+        this.id = id;
+        this.fullname = fullname;
+        this.password = password; // TODO Usar algoritmo Bcrypt para hashing
+        this.funds = 0;
+        this.sell = new ArrayList<Sell>();
+
+    }
+
+    public void add_funds(double funds) {
+        this.funds += funds;
+    }
+
+    public double get_funds() {
+        return this.funds;
+    }
+
+    public void add_sell(Sell sell) {
+        this.sell.add(sell);
+    }
+
+    @Override
+    public String toString() {
+        return "[USER]: "
+            + "\nUsername: " 
+            + this.id 
+            + "\nFull name: "
+            + this.fullname
+            + "\nFunds: "
+            + this.funds;
     }
 }
--- src/main/java/system/Seller.java
+++ src/main/java/system/Seller.java
@@ -1,16 +1,50 @@
 package system;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.HashMap;
 
-public class Seller extends User implements Serializable {
+public class Seller implements Serializable {
 
     private static final long serialVersionUID = -585749592930578838L;
     public HashMap<String, Product> products;
+    
+    // Atributos comuns de cliente e vendedor
+    public final String id;
+    public String username, fullname, password;
+    public ArrayList<Sell> sell;
+    public double funds;
 
     public Seller(String id, String fullname, String password) {
-        super(id, fullname, password);
+        this.id = id;
+        this.fullname = fullname;
+        this.password = password; // TODO Usar algoritmo Bcrypt para hashing
+        this.funds = 0;
+        this.sell = new ArrayList<Sell>();
         // funds remains as zero during creation
         this.products = new HashMap<String, Product>(); // 
+    }
+
+    public void add_funds(double funds) {
+        this.funds += funds;
+    }
+
+    public double get_funds() {
+        return this.funds;
+    }
+
+    public void add_sell(Sell sell) {
+        this.sell.add(sell);
+    }
+
+    @Override
+    public String toString() {
+        return "[USER]: "
+            + "\nUsername: " 
+            + this.id 
+            + "\nFull name: "
+            + this.fullname
+            + "\nFunds: "
+            + this.funds;
     }
 }
